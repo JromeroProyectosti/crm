153278a392dd49d046f2f925bd5e8874cba612cf
Jonathan Romero <jonathan.romero@gmail.com>
2020-12-15 18:16:56 -0300

    cambio en el sistma

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends AbstractController
      */
     public function index(): Response
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
