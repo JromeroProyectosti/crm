3e37ed4424049fff55425ba1d085992a45db7d43
Jonathan Romero <jonathan.romero@gmail.com>
2021-02-24 13:09:29 -0300

    Reparando error en importacion

--- src/Controller/ImportacionController.php
+++ src/Controller/ImportacionController.php
@@ -57,7 +57,7 @@ class ImportacionController extends AbstractController
        if ($form->isSubmitted() && $form->isValid()) {
 
             /** @var UploadedFile $brochureFile */
-            $brochureFile = $form->get('file')->getData();
+            $brochureFile = $form->get('url')->getData();
 
             // this condition is needed because the 'brochure' field is not required
             // so the PDF file must be processed only when a file is uploaded
