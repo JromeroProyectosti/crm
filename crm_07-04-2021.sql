2ccb3a145d62c24aa0cbde50395ae0d22d5d3c69
Jonathan Romero <jonathan.romero@gmail.com>
2021-04-11 20:40:29 -0400

    modulos para terminos de contrato

--- crm_07-04-2021.sql
+++ crm_07-04-2021.sql
@@ -1,2 +1,9 @@
 INSERT INTO `modulo` (`id`, `nombre`, `ruta`, `nombre_alt`, `descripcion`) VALUES (NULL, 'terminos', 'terminos_index', 'Términos de contrato', 'COntratos terminados ');
 ALTER TABLE cuota ADD is_multa TINYINT(1) DEFAULT NULL;
+ALTER TABLE contrato ADD pdf_termino VARCHAR(255) DEFAULT NULL;
+
+INSERT INTO `agenda_status` (`id`, `nombre`, `perfil`, `orden`, `icon`, `color`) VALUES
+(12, 'Desconoce', 0, NULL, NULL, NULL),
+(13, 'Desistió', 0, NULL, NULL, NULL),
+(14, 'Reconsidera', 0, NULL, NULL, NULL),
+(15, 'Ratifica Termino', 0, NULL, NULL, NULL);
