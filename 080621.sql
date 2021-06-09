aad000097add0f970039c9abb70b2467cee0408f
Jonathan Romero <jonathan.romero@gmail.com>
2021-06-08 23:15:36 -0400

    cambios en cobranza y pagos

--- 080621.sql
+++ 080621.sql
@@ -1,3 +1,4 @@
 
 ALTER TABLE contrato ADD fecha_compromiso DATE DEFAULT NULL;
 ALTER TABLE contrato ADD ultima_funcion VARCHAR(255) DEFAULT NULL, ADD q_mov INT DEFAULT NULL;
+ALTER TABLE cobranza ADD fecha DATE DEFAULT NULL;
