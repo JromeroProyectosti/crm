e5a8243c631248cc4b5cbe33a2de26cb41730032
Jonathan Romero <jonathan.romero@gmail.com>
2021-06-24 23:38:00 -0400

    agregamos reporte cobrador

--- 220621.sql
+++ 220621.sql
@@ -5,6 +5,21 @@ ALTER TABLE usuario_lote ADD CONSTRAINT FK_CCE488FDB38439E FOREIGN KEY (usuario_
 ALTER TABLE usuario_lote ADD CONSTRAINT FK_CCE488FB172197C FOREIGN KEY (lote_id) REFERENCES lotes (id);
 ALTER TABLE contrato ADD id_lote_id INT DEFAULT NULL;
 ALTER TABLE contrato ADD CONSTRAINT FK_666965236FEFB00C FOREIGN KEY (id_lote_id) REFERENCES lotes (id);
+
+INSERT INTO `lotes` (`id`, `empresa_id`, `nombre`, `estado`, `orden`, `is_utilizado`, `is_asignado`) VALUES
+(1, 1, '1', 1, 1, 1, 0),
+(2, 1, '2', 1, 2, 1, 0),
+(3, 1, '3', 1, 3, 0, 0),
+(4, 1, '4', 1, 4, 0, 0),
+(5, 1, '5', 1, 5, 0, 0),
+(6, 1, '6', 1, 6, 0, 0),
+(7, 1, '7', 1, 7, 0, 0),
+(8, 1, '8', 1, 8, 0, 0),
+(9, 1, '9', 1, 9, 0, 0),
+(10, 1, '10', 1, 10, 0, 0),
+(11, 1, '11', 1, 11, 0, 0),
+(12, 1, '12', 1, 12, 0, 0);
+
 update contrato set id_lote_id=lote;
 
 ALTER TABLE `contrato` DROP FOREIGN KEY `FK_666965236FEFB00C`; ALTER TABLE `contrato` ADD CONSTRAINT `FK_666965236FEFB00C` FOREIGN KEY (`id_lote_id`) REFERENCES `lotes`(`id`) ON DELETE SET NULL ON UPDATE NO ACTION;
--- src/Controller/ReporteController.php
+++ src/Controller/ReporteController.php
@@ -27,18 +27,31 @@ class ReporteController extends AbstractController
      */
     public function agendador(ContratoRepository $contratoRepository): Response
     {
+        $this->denyAccessUnlessGranted('view','reporte_agendador');
         $contratos=$contratoRepository->findAll();
         return $this->render('reporte/reporte_agendador.html.twig', [
             'controller_name' => 'ReporteController',
             'contratos' => $contratos,
         ]);
     }
-        /**
+     /**
      * @Route("/abogado", name="reporte_abogado", methods={"GET"})
      */
     public function abogado(): Response
     {
+        $this->denyAccessUnlessGranted('view','reporte_abogado');
         return $this->render('reporte/reporte_abogado.html.twig', [
+            'controller_name' => 'ReporteController',
+        ]);
+    }
+
+    /**
+     * @Route("/cobrador", name="reporte_cobrador", methods={"GET"})
+     */
+    public function cobrador(): Response
+    {
+        $this->denyAccessUnlessGranted('view','reporte_cobrador');
+        return $this->render('reporte/reporte_cobrador.html.twig', [
             'controller_name' => 'ReporteController',
         ]);
     }
