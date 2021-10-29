077be053ea69070e2f78a72c23fac96884f423e9
Jonathan Romero <jonathan.romero@gmail.com>
2021-10-29 12:47:34 -0300

    agrega desiste y desconoce en panel abogados

--- src/Controller/PanelAbogadoController.php
+++ src/Controller/PanelAbogadoController.php
@@ -58,7 +58,7 @@ class PanelAbogadoController extends AbstractController
         $compania=null;
         $fecha=null;
         $statues='5';
-        $statuesgroup='4,5,7,6,8,14,15';
+        $statuesgroup='4,5,7,6,8,14,15,12,13';
         $status=null;
         $tipo_fecha=1;
         if(null !== $request->query->get('bFiltro') && trim($request->query->get('bFiltro'))!=''){
